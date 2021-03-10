d8aa4cc8178adc3bb90329adc2b84483263c6215
Mr.Nobody <agwebdes@yandex.ru>
2021-03-10 19:11:56 +0300

    Стилизовал главную страницу под мобильный размер

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './scss/index.scss'
+import $ from 'jquery'
+import 'slick-carousel'
+
+$(".slider").slick({
+  arrows: false,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  dots: true,
+  speed: 700,
+  autoplay: false
+})
